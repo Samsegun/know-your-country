10c4170280240b869e917ff21b4e7d21864c2257
Samsegun <oyebadesamsegun@gmail.com>
2021-11-06 17:08:17 +0100

    filter button arrow rotation

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const filterControl = document.querySelector(".filter-control");
+const filterControlSpan = document.querySelector(".filter-control span");
 const filterDropDown = document.querySelector(".filter-dropdown");
 
 filterControl.addEventListener("click", () => {
   filterDropDown.classList.toggle("slide-down");
+  filterControlSpan.classList.toggle("rotate");
 });
